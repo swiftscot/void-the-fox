24d224a38d10cca53a30d347081d17e17c68c2c7
swiftscot <129270700+swiftscot@users.noreply.github.com>
2023-06-07 10:10:45 +0000

    vtf_leveledit v0.1

--- leveleditor.py
+++ leveleditor.py
@@ -28,10 +28,12 @@ spritesExisting = []
 spritex = []
 spritey = []
 spriteid = []
+entities = ["spawn.png", "ally.png", "enemy.png", "dialogue.png", "sfx.png", "music.png", "video.png"]
 entityx = []
 entityy = []
 entitydata = []
 selectedSprite = -1
+selectedEntity = -1
 bgs = []
 bgsExisting = []
 activeBg = ""
@@ -49,6 +51,7 @@ GREY = (50,50,50)
 BLUE = (51,51,255)
 LBLUE = (102,102,255)
 BLACK = (0,0,0)
+V01 = (255,0,0)
 
 # images
 voiddash = [pygame.image.load("vtf_shared/leveleditor_sprites/voiddash.png"),pygame.image.load("vtf_shared/leveleditor_sprites/voiddash2.png")]
@@ -60,18 +63,18 @@ sublogofont = pygame.font.SysFont("Arial", 20)
 normalfont = pygame.font.SysFont("Arial", 50)
 
 # texts
-logotext = logofont.render("Void the Fox", True, WHITE)
-sublogotext = sublogofont.render("Level Editor prerelease-06Jun23", True, WHITE)
+logotext = logofont.render("Void Engine", True, V01)
+sublogotext = sublogofont.render("Level Editor v0.1 - Barebones", True, V01)
 
 # loop
 while running == True:
   clock.tick(60)
   tick = tick + 1
   keys = pygame.key.get_pressed()
-  mouse_x,mouse_y = pygame.mouse.get_pos()
-  mouse_1,mouse_2,mouse_3 = pygame.mouse.get_pressed(num_buttons=3)
-  pygame.display.set_caption("Void the Fox - Level Editor (loaded game: " + gameLoaded + ") (loaded level: " + currentLevel +") (runtime: " + str(math.trunc(tick/3600)) + " minutes)")
-  editorxytext = sublogofont.render(str(int(round(mouse_x/20) + round(camerax/20)/-1)) + ", " + str(round(mouse_y/20) + round(cameray/20)), True, WHITE)
+  mousex,mousey = pygame.mouse.get_pos()
+  mouse1,mouse2,mouse3 = pygame.mouse.get_pressed(num_buttons=3)
+  pygame.display.set_caption("Void Engine - Level Editor (loaded game: " + gameLoaded + ") (loaded level: " + currentLevel +") (runtime: " + str(math.trunc(tick/3600)) + " minutes)")
+  editorxytext = sublogofont.render(str(int(round(mousex/20) + round(camerax/20)/-1)) + ", " + str(round(mousey/20) + round(cameray/20)), True, WHITE)
   editorpagetext = sublogofont.render("Page: " + str(page), True, WHITE)
   editorspritesusedtext = sublogofont.render("Sprites used: " + str(len(spritex)), True, WHITE)
   editorentitesusedtext = sublogofont.render("Entities used: " + str(len(entityx)), True, WHITE)
@@ -92,9 +95,9 @@ while running == True:
           menu = False
         else:
           menu = True
-      if event.key == pygame.K_LEFT:
+      if event.key == pygame.K_COMMA:
         page = page - 1
-      if event.key == pygame.K_RIGHT:
+      if event.key == pygame.K_PERIOD:
         page = page + 1
       if event.key == pygame.K_PAUSE:
         if "" > 5:
@@ -153,7 +156,9 @@ while running == True:
         saveFile = open(gameLoaded + "/levels/" + saveName + ".vtflevel", "a")
         saveFile.write("bg" + str(activeBgId) + "id\n")
         for counter in range(len(spritex)):
-          saveFile.write("sprite" + str(spriteid[counter]) + "id" + str(spritex[counter]) + "x" + str(spritey[counter]) + "y\n")
+          saveFile.write("sprite" + str(spriteid[counter]) + "id" + str(spritex[counter]) + "xc" + str(spritey[counter]) + "yc\n")
+        for counter in range(len(entitydata)):
+          saveFile.write("entity" + str(entitydata[counter]) + "id" + str(entityx[counter]) + "xc" + str(entityy[counter]) + "yc\n")
         saveFile.close()
       elif saveTyping == True and event.key == pygame.K_BACKSPACE:
         saveName = saveName[:-2]
@@ -166,20 +171,27 @@ while running == True:
         currentLevel = loadName
         loadFile = open(gameLoaded + "/levels/" + loadName + ".vtflevel", "r")
         loadFileContents = loadFile.readlines()
-        if loadFileContents[0] == "bg-1id":
+        if loadFileContents[0] == "bg-1id\n":
           print("no background")
         else:
           activeBg = pygame.image.load(gameLoaded + "/backgrounds/" + bgs[int(loadFileContents[0][2:loadFileContents[0].find("id")])])
           activeBgName = bgs[int(loadFileContents[0][2:loadFileContents[0].find("id")])]
           activeBgId = int(loadFileContents[0][2:loadFileContents[0].find("id")])
-          spriteid = []
-          spritex = []
-          spritey = []
-        for counter in range(len(loadFileContents)-1):
+        spriteid = []
+        spritex = []
+        spritey = []
+        entitydata = []
+        entityx = []
+        entityy = []
+        for counter in range(len(loadFileContents)):
           if loadFileContents[counter].find("sprite") != -1:
             spriteid.append(int(loadFileContents[counter][6:loadFileContents[counter].find("id")]))
-            spritex.append(int(loadFileContents[counter][loadFileContents[counter].find("id")+2:loadFileContents[counter].find("x")]))
-            spritey.append(int(loadFileContents[counter][loadFileContents[counter].find("x")+1:loadFileContents[counter].find("y")]))
+            spritex.append(int(loadFileContents[counter][loadFileContents[counter].find("id")+2:loadFileContents[counter].find("xc")]))
+            spritey.append(int(loadFileContents[counter][loadFileContents[counter].find("xc")+2:loadFileContents[counter].find("yc")]))
+          if loadFileContents[counter].find("entity") != -1:
+            entitydata.append(int(loadFileContents[counter][6:loadFileContents[counter].find("id")]))
+            entityx.append(int(loadFileContents[counter][loadFileContents[counter].find("id")+2:loadFileContents[counter].find("xc")]))
+            entityy.append(int(loadFileContents[counter][loadFileContents[counter].find("xc")+2:loadFileContents[counter].find("yc")]))
       elif loadTyping == True and event.key == pygame.K_BACKSPACE:
         loadName = loadName[:-2]
 
@@ -199,17 +211,23 @@ while running == True:
   if menu == True:
     screen.fill(GREY)
     screen.blit(voiddash[voidsprite], (480,500))
-    if mouse_x > 20 and mouse_x < 130 and mouse_y > 250 and mouse_y < 300: # new
-      if mouse_1 == True and len(gameinfocontent) > 0:
+    if mousex > 20 and mousex < 130 and mousey > 250 and mousey < 300: # new
+      if mouse1 == True and len(gameinfocontent) > 0:
         menu = False
         spritex = []
         spritey = []
         spriteid = []
+        entityx = []
+        entityy = []
+        entitydata = []
+        activeBg = ""
+        activeBgName = ""
+        activeBgId = -1
       menunewtext = normalfont.render("New", True, LGREY)
     else:
       menunewtext = normalfont.render("New", True, WHITE)
-    if mouse_x > 20 and mouse_x < 145 and mouse_y > 300 and mouse_y < 350: # save
-      if mouse_1 == True and len(gameinfocontent) > 0:
+    if mousex > 20 and mousex < 145 and mousey > 300 and mousey < 350: # save
+      if mouse1 == True and len(gameinfocontent) > 0:
         saveTyping = True
         loadTyping = False
         gameTyping = False
@@ -217,8 +235,8 @@ while running == True:
       menusavetext = normalfont.render("Save: " + saveName, True, LGREY)
     else:
       menusavetext = normalfont.render("Save: " + saveName, True, WHITE)
-    if mouse_x > 20 and mouse_x < 140 and mouse_y > 350 and mouse_y < 400: # load
-      if mouse_1 == True and len(gameinfocontent) > 0:
+    if mousex > 20 and mousex < 140 and mousey > 350 and mousey < 400: # load
+      if mouse1 == True and len(gameinfocontent) > 0:
         loadTyping = True
         gameTyping = False
         newGameTyping = False
@@ -226,14 +244,14 @@ while running == True:
       menuloadtext = normalfont.render("Load: " + loadName, True, LGREY)
     else:
       menuloadtext = normalfont.render("Load: " + loadName, True, WHITE)
-    if mouse_x > 20 and mouse_x < 130 and mouse_y > 400 and mouse_y < 450: # quit
-      if mouse_1 == True:
+    if mousex > 20 and mousex < 130 and mousey > 400 and mousey < 450: # quit
+      if mouse1 == True:
         running = False
       menuquittext = normalfont.render("Quit", True, LGREY)
     else:
       menuquittext = normalfont.render("Quit", True, WHITE)
-    if mouse_x > 20 and mouse_x < 180 and mouse_y > 500 and mouse_y < 550: # game
-      if mouse_1 == True:
+    if mousex > 20 and mousex < 180 and mousey > 500 and mousey < 550: # game
+      if mouse1 == True:
         gameTyping = True
         loadTyping = False
         saveTyping = False
@@ -241,8 +259,8 @@ while running == True:
       menugametext = normalfont.render("Game: " + gameLoaded, True, LGREY)
     else:
       menugametext = normalfont.render("Game: " + gameLoaded, True, WHITE)
-    if mouse_x > 20 and mouse_x < 310 and mouse_y > 550 and mouse_y < 600: # game
-      if mouse_1 == True:
+    if mousex > 20 and mousex < 310 and mousey > 550 and mousey < 600: # game
+      if mouse1 == True:
         newGameTyping = True
         loadTyping = False
         saveTyping = False
@@ -251,7 +269,7 @@ while running == True:
     else:
       menunewgametext = normalfont.render("New Game: " + newGame, True, WHITE)
     screen.blit(logotext, (85,50))
-    screen.blit(sublogotext, (168,130))
+    screen.blit(sublogotext, (168,145))
     screen.blit(menunewtext, (20,250))
     screen.blit(menusavetext, (20,300))
     screen.blit(menuloadtext, (20,350))
@@ -262,53 +280,77 @@ while running == True:
     screen.fill(WHITE)
     if activeBg != "":
       screen.blit(activeBg, (0 + math.trunc(camerax/20)*20,0 + math.trunc(cameray/20)*20))
+    if len(entitydata) > 0:
+      for counter in range(len(entitydata)):
+        screen.blit(pygame.image.load("vtf_shared/leveleditor_entities/" + entities[entitydata[counter]]), (entityx[counter] + math.trunc(camerax/20)*20,entityy[counter] + math.trunc(cameray/20)*20))
+    pygame.draw.rect(screen, GREY, (0,480,640,160))
+    screen.blit(voiddash[voidsprite], (480,500))
     if len(spriteid) > 0:
       for counter in range(len(spriteid)):
         screen.blit(pygame.image.load(gameLoaded + "/sprites/" + sprites[spriteid[counter]]), (spritex[counter] + math.trunc(camerax/20)*20,spritey[counter] + math.trunc(cameray/20)*20))
-    pygame.draw.rect(screen, GREY, (0,480,640,160))
-    screen.blit(voiddash[voidsprite], (480,500))
     for counter in range(32):
       pygame.draw.line(screen, (LGREY), (counter*20,0), (counter*20,479))
     for counter in range(24):
       pygame.draw.line(screen, (LGREY), (0,counter*20), (640,counter*20))
-    if mouse_y < 480 and mouse_1 == True and selectedSprite != -1:
+    if mousey < 480 and mouse1 == True and selectedSprite != -1:
       spriteNotOkay = False
       for counter in range(len(spritex)):
-        if spritex[counter] == math.trunc(mouse_x/20)*20 - math.trunc(camerax/20)*20 and spritey[counter] == math.trunc(mouse_y/20)*20 - math.trunc(cameray/20)*20:
+        if spritex[counter] == math.trunc(mousex/20)*20 - math.trunc(camerax/20)*20 and spritey[counter] == math.trunc(mousey/20)*20 - math.trunc(cameray/20)*20:
           spriteNotOkay = True
       if spriteNotOkay == False:
-        spritex.append(math.trunc(mouse_x/20)*20 - math.trunc(camerax/20)*20)
-        spritey.append(math.trunc(mouse_y/20)*20 - math.trunc(cameray/20)*20)
+        spritex.append(math.trunc(mousex/20)*20 - math.trunc(camerax/20)*20)
+        spritey.append(math.trunc(mousey/20)*20 - math.trunc(cameray/20)*20)
         spriteid.append(selectedSprite)
-    if mouse_y < 480 and mouse_3 == True:
+    elif mousey < 480 and mouse1 == True and selectedEntity != -1:
+      entityNotOkay = False
+      for counter in range(len(entityx)):
+        if entityx[counter] == math.trunc(mousex/20)*20 - math.trunc(camerax/20)*20 and entityy[counter] == math.trunc(mousey/20)*20 - math.trunc(cameray/20)*20:
+          entityNotOkay = True
+      if entityNotOkay == False:
+        if selectedEntity == 0:
+          spawnPlaced = False
+          for counter in range(len(entitydata)):
+            if entitydata[counter] == 0:
+              spawnPlaced = True
+          if spawnPlaced == False:
+            entityx.append(math.trunc(mousex/20)*20 - math.trunc(camerax/20)*20)
+            entityy.append(math.trunc(mousey/20)*20 - math.trunc(cameray/20)*20)
+            entitydata.append(0)
+    if mousey < 480 and mouse3 == True:
       if len(spriteid) > 0:
         for counter in range(len(spriteid)):
-          if spritex[counter] == math.trunc(mouse_x/20)*20 - math.trunc(camerax/20)*20 and spritey[counter] == math.trunc(mouse_y/20)*20 - math.trunc(cameray/20)*20:
+          if spritex[counter] == math.trunc(mousex/20)*20 - math.trunc(camerax/20)*20 and spritey[counter] == math.trunc(mousey/20)*20 - math.trunc(cameray/20)*20:
             spritex.pop(counter)
             spritey.pop(counter)
             spriteid.pop(counter)
             break
-    if mouse_x > 5 and mouse_x < 70 and mouse_y > 490 and mouse_y < 510: # sprites
+        for counter in range(len(entitydata)):
+          if entityx[counter] == math.trunc(mousex/20)*20 - math.trunc(camerax/20)*20 and entityy[counter] == math.trunc(mousey/20)*20 - math.trunc(cameray/20)*20:
+            entityx.pop(counter)
+            entityy.pop(counter)
+            entitydata.pop(counter)
+            break
+    if mousex > 5 and mousex < 70 and mousey > 490 and mousey < 510: # sprites
       editorspritestext = sublogofont.render("Sprites", True, LGREY)
-      if mouse_1 == True:
+      if mouse1 == True:
         tab = 1
     else:
       editorspritestext = sublogofont.render("Sprites", True, WHITE)
-    if mouse_x > 85 and mouse_x < 150 and mouse_y > 490 and mouse_y < 510: # entities
+    if mousex > 85 and mousex < 150 and mousey > 490 and mousey < 510: # entities
       editorentitiestext = sublogofont.render("Entities", True, LGREY)
-      if mouse_1 == True:
+      if mouse1 == True:
         tab = 2
     else:
       editorentitiestext = sublogofont.render("Entities", True, WHITE)
-    if mouse_x > 165 and mouse_x < 270 and mouse_y > 490 and mouse_y < 510: # background
+    if mousex > 165 and mousex < 270 and mousey > 490 and mousey < 510: # background
       editorbackgroundstext = sublogofont.render("Background", True, LGREY)
-      if mouse_1 == True:
+      if mouse1 == True:
         tab = 3
     else:
       editorbackgroundstext = sublogofont.render("Background", True, WHITE)
-    if mouse_x > 285 and mouse_x < 340 and mouse_y > 490 and mouse_y < 510: # import
+    if mousex > 285 and mousex < 340 and mousey > 490 and mousey < 510: # import
       editorimporttext = sublogofont.render("Import", True, LGREY)
-      if mouse_1 == True:
+      if mouse1 == True:
         tab = 4
     else:
       editorimporttext = sublogofont.render("Import", True, WHITE)
@@ -325,16 +367,26 @@ while running == True:
     if selectedSprite != -1:
       selectedSpriteImg = pygame.image.load(gameLoaded + "/sprites/" + sprites[selectedSprite])
       screen.blit(selectedSpriteImg, (425, 612))
+    elif selectedEntity != -1:
+      selectedEntityImg = pygame.image.load("vtf_shared/leveleditor_entities/" + entities[selectedEntity])
+      screen.blit(selectedEntityImg, (425, 612))
     if tab == 1:
       for counter in range(len(sprites)):
-        if counter < 20 and page == 1:
-          sprite = pygame.image.load(gameLoaded + "/sprites/" + sprites[counter])
-          screen.blit(sprite, (counter*20,520))
-        elif counter < 20*page and counter >= 20*(page-1):
-          sprite = pygame.image.load(gameLoaded + "/sprites/" + sprites[counter])
-          screen.blit(sprite, (counter*20-page*200,520))
-        if mouse_x > counter*20 and mouse_x < counter*20+20 and mouse_y > 520 and mouse_y < 540 and mouse_1 == True:
+        sprite = pygame.transform.scale(pygame.image.load(gameLoaded + "/sprites/" + sprites[counter]), (40, 40))
+        if counter < 10 and page == 1:
+          screen.blit(sprite, (counter*40,520))
+        elif counter < 10*page and counter >= 10*(page-1):
+          screen.blit(sprite, (counter*40-page*200,520))
+        if mousex > counter*40 and mousex < counter*40+40 and mousey > 520 and mousey < 560 and mouse1 == True:
           selectedSprite = counter
+          selectedEntity = -1
+    if tab == 2:
+      for counter in range(len(entities)):
+        entity = pygame.transform.scale(pygame.image.load("vtf_shared/leveleditor_entities/" + entities[counter]), (40,40))
+        screen.blit(entity, (counter*40,520))
+        if mousex > counter*40 and mousex < counter*40+40 and mousey > 520 and mousey < 560 and mouse1 == True:
+          selectedSprite = -1
+          selectedEntity = counter
     if tab == 3:
       for counter in range(len(bgs)):
         if counter < 6 and page == 1:
@@ -345,14 +397,14 @@ while running == True:
           bg = pygame.image.load(gameLoaded + "/backgrounds/" + bgs[counter])
           bg = pygame.transform.scale(bg, (80,60))
           screen.blit(bg, (counter*80-page*480,520))
-        if mouse_x > counter*80 and mouse_x < counter*80+80 and mouse_y > 520 and mouse_y < 580 and mouse_1 == True:
+        if mousex > counter*80 and mousex < counter*80+80 and mousey > 520 and mousey < 580 and mouse1 == True:
           activeBg = pygame.image.load(gameLoaded + "/backgrounds/" + bgs[counter])
           activeBgName = bgs[counter]
           activeBgId = counter
     if tab == 4:
-      if mouse_x > 5 and mouse_x < 165 and mouse_y > 520 and mouse_y < 540: # import sprites
+      if mousex > 5 and mousex < 165 and mousey > 520 and mousey < 540: # import sprites
         importspritestext = sublogofont.render("Import Sprites", True, LBLUE)
-        if mouse_1 == True:
+        if mouse1 == True:
           gameinfo.close()
           gameinfo = open(gameLoaded + "/gameinfo.txt", "a")
           spritesExisting = os.listdir(gameLoaded + "/sprites")
@@ -379,9 +431,9 @@ while running == True:
               sprites.append(gameinfocontent[counter][gameinfocontent[counter].find("---")+3:-1])
       else:
         importspritestext = sublogofont.render("Import Sprites", True, BLUE)
-      if mouse_x > 5 and mouse_x < 205 and mouse_y > 540 and mouse_y < 560:
+      if mousex > 5 and mousex < 205 and mousey > 540 and mousey < 560:
         importbgtext = sublogofont.render("Import Backgrounds", True, LBLUE)
-        if mouse_1 == True:
+        if mouse1 == True:
           gameinfo.close()
           gameinfo = open(gameLoaded + "/gameinfo.txt", "a")
           bgsExisting = os.listdir(gameLoaded + "/backgrounds")
@@ -408,10 +460,10 @@ while running == True:
               bgs.append(gameinfocontent[counter][gameinfocontent[counter].find("-------")+7:-1])
       else:
         importbgtext = sublogofont.render("Import Backgrounds", True, BLUE)
-      if mouse_x > 5 and mouse_x < 145 and mouse_y > 560 and mouse_y < 580:
-        importmusictext = sublogofont.render("Import Music", True, LBLUE)
+      if mousex > 5 and mousex < 145 and mousey > 560 and mousey < 580:
+        importmusictext = sublogofont.render("Import Media", True, LBLUE)
       else:
-        importmusictext = sublogofont.render("Import Music", True, BLUE)
+        importmusictext = sublogofont.render("Import Media", True, BLUE)
       screen.blit(importspritestext, (5, 520))
       screen.blit(importbgtext, (5, 540))
       screen.blit(importmusictext, (5, 560))
