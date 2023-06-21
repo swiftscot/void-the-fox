8488f18a2558f92081463db7714d714e8ca66532
Connor <129270700+swiftscot@users.noreply.github.com>
2023-06-21 10:22:10 +0100

    draw mainmenu

--- edit.py
+++ edit.py
@@ -1,2 +1,25 @@
 import pygame
 
+pygame.init()
+pygame.font.init()
+screen = pygame.display.set_mode()
+running = True
+mainMenuActive = True
+
+MAINMENULEFT = pygame.image.load("shared/voidmainmenuleft.png")
+MAINMENURIGHT = pygame.image.load("shared/voidmainmenuright.png")
+
+def main_menu(MAINMENULEFT, MAINMENURIGHT):
+  screen.blit(MAINMENULEFT, (0,0))
+  screen.blit(MAINMENURIGHT, (366,0))
+
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+
+  if mainMenuActive:
+    main_menu(MAINMENULEFT, MAINMENURIGHT)
+    
+  pygame.display.update()
+pygame.quit()
