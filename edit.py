4ddd45b2f9c8534a1db428313bf7797144ae5424
Connor <thedefcon6@gmail.com>
2023-06-23 14:36:36 +0100

    file dropdown

--- edit.py
+++ edit.py
@@ -5,15 +5,16 @@ pygame.font.init()
 screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 running = True
-mainMenuActive = True
-newGameActive = False
+fileDropdown = False
+gameLoaded = ""
 
-NAMESMILE = pygame.font.SysFont('Name Smile', 30)
+NAMESMILE = pygame.font.Font('shared/Name Smile.otf', 30)
 WHITE = (255,255,255)
-PURPLE = (196,0,252)
 GREY = (50,50,50)
+PURPLE = (196,0,252)
+GREEN = (0,255,0)
 
-def graphics():
+def graphics(fileDropdown):
 	screen.fill(WHITE)
 	for counter in range(int(round(screenSizeW/10)*10)):
 		pygame.draw.line(screen, GREY, (counter*40,0), (counter*40, screenSizeH))
@@ -23,6 +24,23 @@ def graphics():
 	screen.blit(NAMESMILE.render("File", True, WHITE), (0,0))
 	screen.blit(NAMESMILE.render("Tools", True, WHITE), (100,0))
 	screen.blit(NAMESMILE.render("Help", True, WHITE), (240,0))
+	if fileDropdown:
+		pygame.draw.rect(screen, PURPLE, (0,30,300,240))
+		screen.blit(NAMESMILE.render("New Level", True, WHITE), (0,30))
+		screen.blit(NAMESMILE.render("Save Level", True, WHITE), (0,60))
+		screen.blit(NAMESMILE.render("Load Level", True, WHITE), (0,90))
+		screen.blit(NAMESMILE.render("New Game", True, WHITE), (0,150))
+		screen.blit(NAMESMILE.render("Load Game", True, WHITE), (0,180))
+		screen.blit(NAMESMILE.render("Import Assets", True, WHITE), (0,240))
+	if gameLoaded == "":
+		screen.blit(NAMESMILE.render("You must first load a game. (File > New Game or File > Load Game)", True, GREEN), (350,0))
+
+def input(mouseX,mouseY,mouse1,mouse2):
+	global fileDropdown
+	if mouseX < 100 and mouseY < 30:
+		fileDropdown = True
+	if mouseY > 270 or mouseX > 300:
+		fileDropdown = False
 
 while running:
 	clock.tick(60)
@@ -33,6 +51,7 @@ while running:
 	mouse1,mouse3,mouse2 = pygame.mouse.get_pressed(num_buttons=3)
 	screenSizeW,screenSizeH = screen.get_size()
 
-	graphics()
+	graphics(fileDropdown)
+	input(mouseX,mouseY,mouse1,mouse2)
 	pygame.display.update()
 pygame.quit()
